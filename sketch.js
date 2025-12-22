e1735f90f2e6b4090dd46382c1d1b654984008d1
gaowenling969-spec <gaowenling969@gmail.com>
2025-12-22 04:43:18 -0500

    Enhance sketch with mouse interaction and visuals
    Added interactive elements and dynamic visuals to the sketch.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,46 @@
-//The setup function only happens once
+var sandy = 0;
+var donald = 450;
+
 function setup() {
-	createCanvas(500, 500); //create a 500px X 500px canvas
+  createCanvas(500, 500);
 }
 
-//The draw function happens over and over again
 function draw() {
-  background(255,182,193); pink background
-  //circle
-  stroke(255,255,127) // an RGB color for the circle's border
-  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
-  ellipse(width/2，height/2,100,100); // center of canvas, 20px dia
+  background(173, mouseY, 230);
+
+  stroke(255, 255, 127);
+  fill(255, 255, 127, 150);
+  ellipse(mouseX, 250, sandy + 60, mouseY);
+
+  stroke(189, 109, 183);
+  strokeWeight(5);
+  rect(50, 50, sandy, sandy);
+
+  line(50, donald, 450, sandy);
+
+  fill(109, 189, 113, 225);
+  ellipse(250, 250, 50, 50);
+
+  fill(247, 209, 54);
+  textFont("Impact");
+  textSize(75);
+  text("Partytime!!!!", 100, 250);
+
+  fill(54, mouseX, 247);
+  strokeWeight(0);
+  ellipse(mouseX, mouseY, 50, mouseY);
+}
+
+function mousePressed() {
+  if (sandy >= 255) {
+    sandy = 0;
+  } else {
+    sandy = sandy + 5;
+  }
+
+  if (donald <= 0) {
+    donald = 450;
+  } else {
+    donald = donald - 5;
+  }
 }
